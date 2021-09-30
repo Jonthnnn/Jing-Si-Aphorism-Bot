7e4ca3cc220215b9aba9cb5ddc6b596377fab5a7
Jonathan W <91401975+Jonthnnn@users.noreply.github.com>
2021-09-30 18:55:50 +0000

    Added more aphorisms.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
+//Aphorism Section:
 client.on('message', msg => {
 JSA1 = "./Images/JingSiAphorism1.jpg";
 JSA2 = "./Images/JingSiAphorism2.jpg";
@@ -18,8 +19,14 @@ JSA8 = "./Images/JingSiAphorism8.jpg";
 JSA9 = "./Images/JingSiAphorism9.jpg";
 JSA10 = "./Images/JingSiAphorism10.jpg";
 JSA11 = "./Images/JingSiAphorism11.jpg";
+JSA12 = "./Images/JingSiAphorism12.jpg";
+JSA13 = "./Images/JingSiAphorism13.jpg";
+JSA14 = "./Images/JingSiAphorism14.jpg";
+JSA15 = "./Images/JingSiAphorism15.jpg";
+JSA16 = "./Images/JingSiAphorism16.jpg";
+JSA17 = "./Images/JingSiAphorism16.jpg";
 if (msg.content.startsWith ('.aphorism')) {
-  number = 11;
+  number = 17;
   var random = Math.floor (Math.random() * (number) + 1 - 1) + 1;
     switch(random) {
      case 1: msg.channel.send({files: [JSA1]}); break;
@@ -33,6 +40,12 @@ if (msg.content.startsWith ('.aphorism')) {
      case 9: msg.channel.send({files: [JSA9]}); break;
      case 10: msg.channel.send({files: [JSA10]}); break;
      case 11: msg.channel.send({files: [JSA11]}); break;
+     case 12: msg.channel.send({files: [JSA12]}); break;
+     case 13: msg.channel.send({files: [JSA13]}); break;
+     case 14: msg.channel.send({files: [JSA14]}); break;
+     case 15: msg.channel.send({files: [JSA15]}); break;
+     case 16: msg.channel.send({files: [JSA16]}); break;
+     case 17: msg.channel.send({files: [JSA17]}); break;
     }
   }
 });
